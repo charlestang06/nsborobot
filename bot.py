40d3b32f39a04106877d0ce224585bd39dd0f130
Charles Tang <75450612+charlestang06@users.noreply.github.com>
2021-11-01 14:13:29 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1366,9 +1366,8 @@ async def schedule(ctx, arg1, arg2, arg3, arg4, arg5, arg6, arg7):
                         new_schedule[6] + "\n9:35-10:25 AM", (255, 255, 255),
                         font=txtfont)
 
-    im2 = im2.save("temp.png")
-    im2.save(f".//schedules//{ctx.author.id}.png")
-    await ctx.send(file=discord.File(".//temp.png"))
+    im2 = im2.save(f".//schedules//{ctx.author.id}.png")
+    await ctx.send(file=discord.File(f".//schedules//{ctx.author.id}.png"))
 	
 
 @bot.command()
